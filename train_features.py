6ba95e4c704ba98b892feeb16a9fa8dca4077e33
Sherkhan <sherkhanazim@gmail.com>
2021-09-13 09:28:11 +0600

    add train function

--- train_features.py
+++ train_features.py
@@ -1,6 +1,20 @@
 import os
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+
+def train_features(pd_data):
+    x = pd_data.iloc[:, 2:]
+    y = pd_data.iloc[:, 1]
+    X_train, X_test, y_train, y_test = train_test_split(x, y.ravel(), test_size=0.2,
+                                                        stratify=y, random_state=1)
+    elf = ExtraTreesClassifier(n_estimators=100, random_state=0)
+    elf.fit(X_train, y_train)
+    print("Accuracy with 40 points:", elf.score(X_test, y_test))
 
 
 def load_data(dir_path):
@@ -14,6 +28,6 @@ def load_data(dir_path):
                 print(len(d1))
                 data = np.concatenate((data, d1))
     pd_data = pd.DataFrame(data)
-    print(pd_data)
+    train_features(pd_data)
 
 load_data("data")
